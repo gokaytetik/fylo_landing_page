be037184e0e13815b947be678c274f3e88425101
gokaytetik <devgokay@gmail.com>
2021-10-19 02:35:31 +0300

    Boilerplate

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+//Style
+import "./style/boilerplate.scss";
+import "./style/global.scss";
+
+//Components
+
+function App() {
+  return (
+    <React.Fragment>
+      <div>hey</div>
+    </React.Fragment>
+  );
+}
+
+export default App;
